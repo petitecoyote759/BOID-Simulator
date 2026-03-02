40b48cc895c6d2e18c0ce6187d6dea75734b92ef
Mattie <matthew.short.nh@gmail.com>
2026-03-02 23:26:46 +0000

    Added rendering and entity rendering for Renderer

--- Renderer.cs
+++ Renderer.cs
@@ -1,9 +1,10 @@
-﻿using ShortTools.PlanetaryForge;
-using Silk.NET.SDL;
+﻿using BOIDSimulator.ECS_Components;
+using ShortTools.PlanetaryForge;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ShortTools.General;
 using System.Threading.Tasks;
 
 namespace BOIDSimulator
@@ -12,37 +13,66 @@ namespace BOIDSimulator
     {
         public static bool running = true;
 
-        private static ManualResetEvent tasksDone = new(false);
+        private static bool first = true;
         internal static void MainLoop()
         {
-            while (running)
+            if (Map.tileMap is null) { return; }
+            if (running == false) { return; }
+
+            int width = Map.tileMap.Length / drawGridSize;
+            int height = Map.tileMap[0].Length / drawGridSize;
+
+            if (first)
             {
-                foreach (Action task in tasks)
+                first = false;
+                General.debugger.AddLog($"Starting full map render at {DateTimeOffset.Now.ToUnixTimeMilliseconds()} with dimentions {width}x{height}", WarningLevel.Debug);
+                for (int x = 0; x < width; x++)
                 {
-                    task();
+                    for (int y = 0; y < height; y++)
+                    {
+                        DrawGrid(x, y);
+                    }
                 }
-                tasksDone.Reset();
-                tasksDone.WaitOne();
+                General.debugger.AddLog($"Completed full map render at {DateTimeOffset.Now.ToUnixTimeMilliseconds()}", WarningLevel.Debug);
+            }
+
+            while (tasks.Count > 0)
+            {
+                Action task;
+                lock (tasks)
+                {
+                    task = tasks.Dequeue();
+                }
+                task();
             }
         }
 
 
 
 
-        
 
+        static Dictionary<(int, int), HashSet<int>> entitiesToDraw = new Dictionary<(int, int), HashSet<int>>();
         static Queue<Action> tasks = new Queue<Action>();
-        internal static void RequestDrawGrid(int gridX, int gridY)
+        public static void RequestDrawGrid(int gridX, int gridY)
         {
-            tasks.Enqueue(() => DrawGrid(gridX, gridY));
-            tasksDone.Set();
+            Action task = () => DrawGrid(gridX, gridY);
+            lock (tasks)
+            {
+                tasks.Enqueue(task);
+            }
+        }
+        public static void RequestEntityDraw(int gridX, int gridY, int uid)
+        {
+            if (entitiesToDraw.ContainsKey((gridX, gridY)) == false) { entitiesToDraw.Add((gridX, gridY), new HashSet<int>()); }
+            entitiesToDraw[(gridX, gridY)].Add(uid);
         }
 
 
         public const int drawGridSize = 8;
+        const int boidSize = 2;
         private static void DrawGrid(int gridX, int gridY)
         {
-            if (Map.tileMap is null || Map.altitudeMap is null) { return; }
+            if (Map.tileMap is null || Map.altitudeMap is null) { General.debugger.AddLog($"Attempted to draw grid {gridX}x{gridY} when map was null", WarningLevel.Warning); return; }
 
             int width = Map.tileMap.Length;
             int height = Map.tileMap[0].Length;
@@ -56,7 +86,16 @@ namespace BOIDSimulator
                     byte r = (byte)(colours.Item1 * ((3 + Map.altitudeMap[x][y]) / 4f));
                     byte g = (byte)(colours.Item2 * ((3 + Map.altitudeMap[x][y]) / 4f));
                     byte b = (byte)(colours.Item3 * ((3 + Map.altitudeMap[x][y]) / 4f));
-                    renderer.SetPixel(x * size, y * size, size, size, r, g, b);
+                    General.renderer.SetPixel(x * size, y * size, size, size, r, g, b);
+                }
+            }
+
+            if (entitiesToDraw.ContainsKey((gridX, gridY)))
+            {
+                HashSet<int> entities = entitiesToDraw[(gridX, gridY)];
+                foreach (int uid in entities)
+                {
+                    RenderBoid(uid);
                 }
             }
         }
@@ -69,5 +108,24 @@ namespace BOIDSimulator
             { TileID.Grass, new Tuple<byte, byte, byte>(10, 130, 10) },
             { TileID.Forest, new Tuple<byte, byte, byte>(15, 115, 20) }
         };
+
+
+
+
+        private static void RenderBoid(int uid)
+        {
+            EC_Entity? entityData = (EC_Entity?)ECSHandler.ECSs[typeof(EC_Entity)][uid];
+            if (entityData is null) { General.debugger.AddLog($"Entity {uid} has no entity data, flagged {ECSHandler.entities[uid]} in the entity table.", WarningLevel.Error); return; }
+
+            General.renderer.SetPixel(
+            (int)(entityData.Value.position.X * General.PPT),
+            (int)(entityData.Value.position.Y * General.PPT),
+            boidSize * General.PPT,
+            boidSize * General.PPT,
+            255,
+            200,
+            100
+            );
+        }
     }
 }
