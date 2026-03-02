673c3f6c7c1b22918b3202220aacf5583918e9f6
Mattie <matthew.short.nh@gmail.com>
2026-03-02 21:30:12 +0000

    Created despawning Logic

--- ECS Components/Despawning.cs
+++ ECS Components/Despawning.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using ShortTools.General;
 
 namespace BOIDSimulator.ECS_Components
 {
@@ -11,20 +13,76 @@ namespace BOIDSimulator.ECS_Components
         // <<Requirements>> //
         // EC_Entity //
 
+        // <<Public Variables>> //
+
         public float rangeSquared;
 
         public bool Active { get => active; set => active = value; }
         private bool active = true;
 
+
+
+        // <<Constants>> //
+
+        const float totalBoidLifespanSeconds = 120;
+
+
+
+        // <<Modified Constants>> //
+
+        const long totalBoidLifespan = (int)(1000 * totalBoidLifespanSeconds);
+
+
+
+
+        // <<Private Variables>> //
+
+        private static int targetX = 0; // to be updated
+        private static int targetY = 0;
+
+
+        private readonly long creationTime;
+
+
+
+
+
+
         public EC_Despawning(float rangeSquared)
         {
             this.rangeSquared = rangeSquared;
+            this.creationTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
         }
 
 
         public readonly void Action(float dt, int uid)
         {
-            // Delete at centre
+            EC_Entity? Me = (EC_Entity?)ECSHandler.ECSs[typeof(EC_Entity)][uid];
+            if (Me is null) { General.debugger.AddLog($"Error, entity {uid} has no entity data!", WarningLevel.Error); }
+
+#pragma warning disable CS8629 // Me cannot be null at this point
+            Vector2 pos = Me.Value.position;
+#pragma warning restore CS8629
+
+            int tileX = (int)(pos.X / General.boidGridSize);
+            int tileY = (int)(pos.Y / General.boidGridSize);
+
+            // <<Destroy at Centre>> //
+
+            if (MathF.Abs(targetX - tileX) + MathF.Abs(targetY - tileY) < rangeSquared)
+            {
+                ECSHandler.FreeUID(uid);
+                return;
+            }
+
+
+            // <<Destroy After Lifespan>> //
+
+            if (DateTimeOffset.Now.ToUnixTimeMilliseconds() - creationTime > totalBoidLifespan)
+            {
+                ECSHandler.FreeUID(uid);
+                return;
+            }
         }
     }
 }
