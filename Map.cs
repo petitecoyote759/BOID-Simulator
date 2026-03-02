13564ed616af744757e5240312a8242fbd476449
Mattie <matthew.short.nh@gmail.com>
2026-03-02 21:15:23 +0000

    Changed map renderer to be a map class that contains the map data

---
+++ Map.cs
@@ -0,0 +1,23 @@
+﻿using ShortTools.Perlin;
+using SimpleGraphicsLib;
+using System.Net.Mail;
+using ShortTools.PlanetaryForge;
+using System.Runtime.CompilerServices;
+
+namespace BOIDSimulator
+{
+    internal static class Map
+    {
+        public static float[][]? altitudeMap;
+        public static TileID[][]? tileMap;
+
+
+
+        const int scale = 1;
+        public static void CreateMap(int width, int height)
+        {
+            (TileID[][], float[][]) mapData = MapGenerator.CreateMap(width / scale, height / scale);
+            tileMap = mapData.Item1; altitudeMap = mapData.Item2;
+        }
+    }
+}
