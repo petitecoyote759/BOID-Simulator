98f55a6bf801bb74a726e9c3b4e60d671fa3d7e5
Mattie <matthew.short.nh@gmail.com>
2026-03-05 11:58:53 +0000

    Added spawner and spawned logic components

---
+++ ECS Components/SpawnedLogic.cs
@@ -0,0 +1,34 @@
+﻿using ShortTools.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOIDSimulator.ECS_Components
+{
+    internal struct EC_SpawnedLogic : IEntityComponent
+    {
+        public bool Active { get => active; set => active = value; }
+        private bool active = false;
+
+        int spawnerUid = 0;
+        public EC_SpawnedLogic(int spawnerUid) { this.spawnerUid = spawnerUid; }
+
+
+        public void Action(float dt, int uid) { }
+
+        public void Cleanup(int uid)
+        {
+            if (ECSHandler.entities[spawnerUid] == false) { General.debugger.AddLog($"Attempted to access closed spawner.", WarningLevel.Debug); return; } // spawner is closed
+
+            EC_SpawnerLogic? spawnerLogicNullable = (EC_SpawnerLogic?)ECSHandler.ECSs[typeof(EC_SpawnerLogic)][spawnerUid];
+
+            if (spawnerLogicNullable is null) { General.debugger.AddLog($"Spawner logic was null? Has it been replaced?", WarningLevel.Debug); return; }
+
+            EC_SpawnerLogic spawnerLogic = (EC_SpawnerLogic)spawnerLogicNullable;
+            spawnerLogic.current--;
+            ECSHandler.ECSs[typeof(EC_SpawnerLogic)][spawnerUid] = spawnerLogic;
+        }
+    }
+}
---
+++ ECS Components/Spawner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOIDSimulator.ECS_Components
+{
+    internal struct EC_SpawnerLogic : IEntityComponent
+    {
+        public bool Active { get => active; set => active = value; }
+        private bool active = true;
+
+        public const int max = 10000;
+        public int current = 0;
+        public float MaxSpawnsPerSecond 
+        { 
+            get => maxSpawnsPerSecond; 
+            set 
+            { 
+                maxSpawnsPerSecond = value;
+                secondsPerSpawn = 1f / value;
+            } 
+        }
+        private float maxSpawnsPerSecond = 10f; // should only be set via the property
+
+        private Func<int> creatorFunc;
+        private float spawnTimer = 0;
+        private float secondsPerSpawn = 0;
+
+        public EC_SpawnerLogic(Func<int> creatorFunc, float maxSpawnsPerSecond) 
+        { 
+            this.creatorFunc = creatorFunc;
+            this.MaxSpawnsPerSecond = maxSpawnsPerSecond;
+        }
+
+        public void Action(float dt, int uid)
+        {
+            spawnTimer += dt;
+            if (current >= max) { return; }
+            while (spawnTimer > secondsPerSpawn)
+            {
+                spawnTimer -= secondsPerSpawn;
+                int spawnedUid = creatorFunc();
+
+                current++;
+            }
+        }
+
+        public void Cleanup(int uid)
+        {
+
+        }
+    }
+}
