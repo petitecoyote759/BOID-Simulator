76bccdc665fd9a6f18147a4437500ce12851c734
Mattie <matthew.short.nh@gmail.com>
2026-03-05 12:05:21 +0000

    Added the walker spawner

--- Walker.cs
+++ Walker.cs
@@ -17,7 +17,7 @@ namespace BOIDSimulator
 
         private const int deletionRadiusSquared = deletionRadius * deletionRadius;
 
-        public static int CreateWalker(Vector2 position)
+        public static int CreateWalker(Vector2 position, int spawnerUid = -1)
         {
             int uid = ECSHandler.GetUID();
 
@@ -34,17 +34,30 @@ namespace BOIDSimulator
             ECSHandler.ECSs[typeof(EC_Entity)][uid] = Me;
             ECSHandler.ECSs[typeof(EC_Despawning)][uid] = new EC_Despawning(deletionRadiusSquared);
             ECSHandler.ECSs[typeof(EC_BoidLogic)][uid] = new EC_BoidLogic();
-            ECSHandler.ECSs[typeof(EC_Pathfinding)][uid] = new EC_Pathfinding();
+            ECSHandler.ECSs[typeof(EC_PathFinding)][uid] = new EC_PathFinding(Walkable);
             ECSHandler.ECSs[typeof(EC_Render)][uid] = new EC_Render(IntPtr.Zero);
+            ECSHandler.ECSs[typeof(EC_SpawnedLogic)][uid] = new EC_SpawnedLogic(spawnerUid);
 
             // <<Disable the EAF modules used for leaders>> //
 #pragma warning disable CS8602 // This should absolutely not be null as it is assigned just a few lines up
-            ECSHandler.ECSs[typeof(EC_Pathfinding)][uid].Active = false;
+            ECSHandler.ECSs[typeof(EC_PathFinding)][uid].Active = false;
 #pragma warning restore CS8602
 
 
 
             return uid;
+        }
+
+
+        private static bool Walkable(int x, int y)
+        {
+            // <<Bounds Checks>> //
+            if (Map.tileMap is null) { return false; }
+            if (x < 0 || y < 0) { return false; }
+            if (x >= Map.tileMap.Length) { return false; }
+            if (y >= Map.tileMap[0].Length) { return false; }
+
+            return General.Walkable(Map.tileMap[x][y]);
         }
     }
 }
---
+++ WalkerSpawner.cs
@@ -0,0 +1,63 @@
+﻿using BOIDSimulator.ECS_Components;
+using Silk.NET.SDL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOIDSimulator
+{
+    internal static class WalkerSpawner
+    {
+        public const float walkSpeed = 2f;
+        public const int deletionRadius = 10;
+
+        private const int deletionRadiusSquared = deletionRadius * deletionRadius;
+
+        public static int CreateWalkerSpawner()
+        {
+            int uid = ECSHandler.GetUID();
+
+            ECSHandler.entities[uid] = true;
+
+
+
+            // <<Set Variables>> //
+            EC_Entity Me = new EC_Entity();
+            Me.position = new Vector2(0, 0);
+
+
+            // <<EAF Creation>> //
+            ECSHandler.ECSs[typeof(EC_Entity)][uid] = Me;
+            ECSHandler.ECSs[typeof(EC_SpawnerLogic)][uid] = new EC_SpawnerLogic(() => CreateWalker(uid), 100);
+
+
+
+
+            return uid;
+        }
+
+
+        private static Random random = new Random();
+        private static int CreateWalker(int spawnerUid)
+        {
+            if (Map.tileMap is null) { return -1; }
+
+            int position = random.Next(0, 4); // 0-3, going NESW
+
+            int x = 0;
+            int y = 0;
+
+            if (position == 0) { y = Map.tileMap[0].Length - 1; x = random.Next(0, Map.tileMap.Length); } // north
+            if (position == 1) { y = random.Next(Map.tileMap[0].Length); x = Map.tileMap.Length - 1; } // east
+            if (position == 2) { y = 1; x = random.Next(0, Map.tileMap.Length); } // south
+            if (position == 3) { y = random.Next(0, Map.tileMap[0].Length); x = 1; } // west
+
+            int walkerUid = Walker.CreateWalker(new Vector2(x, y), spawnerUid);
+
+            return walkerUid;
+        }
+    }
+}
